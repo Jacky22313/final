546ec923e4cc25dce870024706a96ce4f14fb65e
Jacky <coolybabe23@gmail.com>
2022-11-08 23:36:10 +0800

    新增場景一和二 場景二有炮台 射到玩家會重來

---
+++ Assets/Scripts/shootEnemy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootEnemy : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject bulletPrefab; // 子彈來源 (在Unity介面中拖入)
+    public GameObject firePoint; // 子彈來源 (在Unity介面中拖入)
+    
+    private CharacterController controller;
+
+    void Start()
+    {
+        
+        controller = GetComponent<CharacterController>();
+
+        // 開始一直射擊的 Coroutine 函式
+        StartCoroutine(KeepShooting());
+    }
+
+    void Update()
+    {
+        
+        
+    }
+    void Fire()
+    {
+        Instantiate(bulletPrefab, firePoint.transform.position, transform.rotation);
+    }    
+    
+    IEnumerator KeepShooting()
+    {
+        while (true)
+        {
+            // 射擊
+            Fire();
+
+            // 暫停 0.5 秒
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
